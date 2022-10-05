b3e9d70a778a74849cdb639cef1a9b6247bf8eba
Tim Turvey <tturvey@hotmail.co.uk>
2022-10-05 15:22:44 +0100

    Empty test

---
+++ src/test/java/EmptyTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class EmptyTest {
+
+    @Test
+    void
+    empty_test() {
+        assertEquals(1, 1);
+    }
+
+}
